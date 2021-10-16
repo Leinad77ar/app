4019c4f5cc9ba5cca0e1e92fd52aab49c67fcc8f
Leinad77ar <dxcordoba@outlook.com>
2021-10-16 06:08:34 -0300

    Agregado de productos de la API de Mercado Libre

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,17 +1,18 @@
-const CardComponent = ({title, img}) => {
-
-    const onClick = (event) => {
-        console.log("se clickeo", event)
-    }
-        return (
-        <div className="card col-4">
-            <img className="card-img-top" src={img} alt="imagen" />
-            <div className="card-body">
-                 <h5 className="card-title">{title}</h5>
-                 <p className="card-text">El Tiger I, cuya designación oficial era Panzer VI Tiger, fue un tanque pesado alemán desarrollado en 1941 y usado en la Segunda Guerra Mundial hasta la aparición del Tiger II</p>
-                 <button onClick={onClick} className="btn btn-primary">Comprar</button>
-            </div>
-        </div>
-    )
-}
+const CardComponent = ({ product, addToCart }) => {
+  return (
+    <div className="card col-4">
+      <img className="card-img-top" src={product.thumbnail} alt="imagen" />
+      <div className="card-body">
+        <h5 className="card-title">{product.title}</h5>
+        <p className="card-text">{product.price}</p>
+        <button
+          onClick={(event) => addToCart(event, product)}
+          className="btn btn-primary"
+        >
+          Comprar
+        </button>
+      </div>
+    </div>
+  );
+};
 export default CardComponent;
--- src/containers/ProductsContainer.jsx
+++ src/containers/ProductsContainer.jsx
@@ -1,24 +1,31 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 import CardComponent from "../components/CardComponent";
 const ProductsContainer = () => {
-    const [cart, setCart] = useState ([]);
-    const [products, setProducts] = useState([]);
-    useEffect(async () => {
-        let data = await fetch("https://api.mercadolibre.com/sites/MLA/search?q=remeras estampadas");
-        let response = await data.json();
-        console.log(response.results);
-    })
-    return (
-        <div className="container">
-            <div className="row">
-                <CardComponent title={"Tiger Tank"} img="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg"/>
-                <CardComponent title={"Tiger Tank"} img="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg"/>
-                <CardComponent title={"Tiger Tank"} img="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg"/>
-                <CardComponent title={"Tiger Tank"} img="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg"/>
-                <CardComponent title={"Tiger Tank"} img="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg"/>
-                <CardComponent title={"Tiger Tank"} img="https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_101I-299-1805-16%2C_Nordfrankreich%2C_Panzer_VI_%28Tiger_I%29_cropped.jpg"/>
-            </div>
-        </div>
-    )
-}
+  //   const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  useEffect(async () => {
+    let data = await fetch(
+      "https://api.mercadolibre.com/sites/MLA/search?q=remera de rock"
+    );
+    let response = await data.json();
+    setProducts(response.results);
+    console.log(response.results);
+  }, []);
+  const addToCart = (event, product) => console.log("se compro", product);
+  return (
+    <div className="container">
+      <div className="row">
+        {products.map((element) => {
+          return (
+            <CardComponent
+              key={element.id}
+              product={element}
+              addToCart={addToCart}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default ProductsContainer;
